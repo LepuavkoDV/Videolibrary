2166734dc1b792a0722efb7968f07ddb6d409f48
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2020-08-28 14:46:32 +0300

    deploy to Heroku

--- server/system/database.js
+++ server/system/database.js
@@ -10,7 +10,8 @@ class Database {
   }
 
   async connect () {
-    const result = await mongoose.connect(`mongodb://${this.user}:${this.password}@${this.host}:${this.port}/${this.dbName}`, { useNewUrlParser: true })
+    // const result = await mongoose.connect(`mongodb://${this.user}:${this.password}@${this.host}:${this.port}/${this.dbName}`, { useNewUrlParser: true })
+    const result = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@videolibrary.wypgn.mongodb.net/videolibrary?retryWrites=true&w=majority`)
     return result
   }
 }
